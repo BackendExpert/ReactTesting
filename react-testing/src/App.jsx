19428aed33cab955e17c58808795a54d1fd15994
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-24 22:15:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- react-testing/src/App.jsx
+++ react-testing/src/App.jsx
@@ -1,7 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
   )
 }
