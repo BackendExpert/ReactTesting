4a25de975ac52c5971344a481cd652411a396d91
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 00:34:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- react-testing/src/App.jsx
+++ react-testing/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Testing from "./docs/Testing";
-import IconTest from "./docs/IconTest";
 import NewIcons from "./docs/NewIcons";
 
 export default function App() {
@@ -8,7 +7,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Testing />}/>
-        <Route path="/IconTest" element={<IconTest />} />
         <Route path="/NewIcons" element={<NewIcons />} />
         
       </Routes>
--- react-testing/src/docs/NewIcons.jsx
+++ react-testing/src/docs/NewIcons.jsx
@@ -1,8 +1,50 @@
+
 import React from 'react'
+import {
+     ZeroNumberFill,
+     ZeroNumberSqureFill,
+     OneNumberFill,
+     OneNumberSqureFill,
+     OneTwoThree,
+     ZeroNumberOutLine, 
+     ZeroNumberSqureOutLine,
+     OneNumberOutline,
+     OneNumberSqureOutLine,
+
+     LogoAlexa,
+     LogoAlipay,
+     LogoAmazon,
+     LogoAmd
+
+} from '@jehankandy/jkreact-icons';
+import {ButtonBlue, ButtonRed, ButtonGray, ButtonGreen, ButtonInfo, ButtonYellow} from '@jehankandy/react-jkcss'
+
 
 const NewIcons = () => {
   return (
-    <div>NewIcons</div>
+    <div>
+        <h1>NewIcons</h1>
+        <ZeroNumberFill color={"red"} size={200} />
+        <ZeroNumberSqureFill color={"red"} size={200} />
+        <OneNumberFill color={"red"} size={200} />
+        <OneNumberSqureFill color={"red"} size={200} />
+        <OneTwoThree color={"red"} size={200} />
+        <ZeroNumberOutLine color={"red"} size={200} />
+        <ZeroNumberSqureOutLine color={"red"} size={200} />
+        <OneNumberOutline color={"red"} size={200} />
+        <OneNumberSqureOutLine color={"red"} size={200} />
+
+
+        <LogoAlexa color={"red"} size={200} />
+        <LogoAlipay color={"red"} size={200} />
+        <LogoAmazon color={"red"} size={200} />
+        <LogoAmd color={"red"} size={200} />
+
+        <ButtonBlue>
+            HI all
+        </ButtonBlue>
+
+    </div>
   )
 }
 
