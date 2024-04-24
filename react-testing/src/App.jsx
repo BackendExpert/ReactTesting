75f2124a312aa7496a06fb0663eded4e384d5c83
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-24 22:15:57 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- react-testing/src/App.jsx
+++ react-testing/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Testing from "./docs/Testing";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Testing />}/>
       </Routes>
     </BrowserRouter>
   )
