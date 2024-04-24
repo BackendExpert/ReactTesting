8928925915c1943c0f4d849009d57feabcb7959d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-24 22:33:57 +0530

    Update Testing.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- react-testing/src/docs/Testing.jsx
+++ react-testing/src/docs/Testing.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {ButtonBlue, ButtonRed, ButtonGray, ButtonGreen, ButtonInfo, ButtonYellow} from '@jehankandy/react-jkcss'
 
 const Testing = () => {
   return (
-    <div>Testing</div>
+    <div>
+        <h1>JKButtons</h1>
+
+        <ButtonBlue>
+            HI all
+        </ButtonBlue>
+        <ButtonGray>
+            HI all
+        </ButtonGray>
+        <ButtonRed>
+            HI all
+        </ButtonRed>
+        <ButtonGreen>
+            HI all
+        </ButtonGreen>
+        <ButtonInfo>
+            HI all
+        </ButtonInfo>
+        <ButtonYellow>
+            HI all
+        </ButtonYellow>
+
+    </div>
   )
 }
 
