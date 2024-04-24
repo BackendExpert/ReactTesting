d7036c1794769a13c0bee8122df1fe3d9f1dcbe5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-24 23:27:41 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- react-testing/src/docs/IconTest.jsx
+++ react-testing/src/docs/IconTest.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import { ZeroNumberFill } from 'react-icons';
+import {
+     ZeroNumberFill,
+     ZeroNumberSqureFill,
+     OneNumberFill,
+     OneNumberSqureFill,
+     OneTwoThree,
+     ZeroNumberOutLine, 
+     ZeroNumberSqureOutLine,
+     OneNumberOutline,
+     OneNumberSqureOutLine
+} from 'react-icons';
 
 const IconTest = () => {
   return (
     <div>
         <h1>IconTest</h1>
+        <ZeroNumberFill color={"black"} size={150}/>
+        <ZeroNumberFill color={"black"} size={150}/>
+        <ZeroNumberFill color={"black"} size={150}/>
+        <ZeroNumberFill color={"black"} size={150}/>
+        <ZeroNumberFill color={"black"} size={150}/>
+        <ZeroNumberFill color={"black"} size={150}/>
+        <ZeroNumberFill color={"black"} size={150}/>
+        <ZeroNumberFill color={"black"} size={150}/>
+        <ZeroNumberFill color={"black"} size={150}/>
         <ZeroNumberFill color={"black"} size={150}/>
     </div>
   )
